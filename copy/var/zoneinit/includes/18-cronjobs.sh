0b091a7b1f8010ef359e8885f095d6f9619f8d07
jfqd <jfqd@blun.org>
2018-07-11 16:03:55 +0200

    do not send cron mails on failure

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,5 +1,5 @@
 # add passenger related cronjobs
-CRON='0 3 * * * rm /var/tmp/buffer.*
+CRON='0 3 * * * rm /var/tmp/buffer.* 2>/dev/null
 0,10,20,30,40,50 * * * * /usr/bin/mv /tmp/passenger-error* /var/tmp 2>/dev/null || true
 '
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
