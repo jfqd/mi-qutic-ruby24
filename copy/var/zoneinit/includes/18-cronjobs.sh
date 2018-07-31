49d5555b108a3a13a3ebe752e8ada9a54396d174
jfqd <jfqd@blun.org>
2018-07-31 10:40:16 +0200

    add spiped config and switch to 2018Q2

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,5 +1,6 @@
 # add passenger related cronjobs
-CRON='0 3 * * * rm /var/tmp/buffer.* 2>/dev/null
+CRON='0 3 * * * rm /var/tmp/buffer.* 2>/dev/null || true
 0,10,20,30,40,50 * * * * /usr/bin/mv /tmp/passenger-error* /var/tmp 2>/dev/null || true
+15 3  * * 0 /usr/sbin/svcadm restart apache
 '
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
